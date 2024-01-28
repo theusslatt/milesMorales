ca17fd388fe30fea49e3c3fe0e5356e580ff232d
Matheus Silveira <matheus@exemplo.com>
2024-01-28 12:33:50 -0300

    section assista agora

--- main.js
+++
@@ -1,6 +0,0 @@
-const hamburguer = document.querySelector('header__hamburguer')
-const nav = document.querySelector('header__nav__links')
-
-hamburguer.addEventListener('click', function (nav){
-    nav.target.classList.add('header__nav__links--is-hidden')
-})
